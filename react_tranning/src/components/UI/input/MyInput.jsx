2a12a95f4343c3c72a8a27b6d6d18908306ec8df
SlavaOgnev <113515541+SlavaOgnev@users.noreply.github.com>
2025-03-24 20:09:34 +0300

    Delete react_tranning directory

--- react_tranning/src/components/UI/input/MyInput.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './MyInput.module.css';
-const MyInput =React.forwardRef( (props, ref) => {
-    return (
-        <input {...props} className={classes.myInput}></input>
-    );
-});
-export default MyInput;
